91c65679e25f74cca761e4fc35a567b1e9fc6457
souvik das <souvik8582@gmail.com>
2023-11-11 09:17:08 +0530

    login forntend

--- app.js
+++ app.js
@@ -5,12 +5,19 @@ const db=require("./database/db")
 app.use(express.urlencoded({extended: true}))
 app.use(express.json())
 app.use(express.static(path.join(__dirname,"public")))
+app.get('/',(req,res)=>{
+    res.send("Welcome")
+})
 
-app.get("/",(req,res)=>{
+app.get("/signup",(req,res)=>{
     res.sendFile(__dirname+"/public/signup.html")
 })
 
-app.post("/",(req,res)=>{
+app.get("/login",(req,res)=>{
+    res.sendFile(__dirname+"/public/login.html")
+})
+
+app.post("/signup",(req,res)=>{
     const {username,email,password} = req.body
     const sql="insert into users(username,email,password) values(?,?,?)"
     db.query(sql,[username,email,password],(err)=>{
@@ -19,13 +26,36 @@ app.post("/",(req,res)=>{
             
         }
         else{
-            res.sendFile(__dirname+"/public/login.html")
+            res.redirect('/login')
         }
     })
 
 
 
 })
+
+app.post("/login",(req,res)=>{
+    const {email,password} = req.body
+    const sql="select * from users where email=? and password=?"
+    db.query(sql,[email,password],(err,result)=>{
+        if(err){
+            console.log(err)
+        }
+        else{
+            if(result.length>0){
+                
+                res.redirect("/dashboard")
+            }
+            else{
+               
+                res.redirect("/login")
+            }
+        }
+    })
+})
+ app.get('/dashboard',(req,res)=>{
+    res.send("welcom to dashboard")
+ })
 
 
 
